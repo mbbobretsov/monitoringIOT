4139d72552bd5e465ad4ebb2f88ff84687889c45
mbbobretsov <99724550+mbbobretsov@users.noreply.github.com>
2022-10-02 13:04:47 +0300

    Delete state.h

--- state.h
+++
@@ -1,8 +0,0 @@
-#include "WString.h"
-#pragma once;
-class State {
-  public:
-    virtual String doSomething(const String &str = "") = 0;
-    virtual bool isNext() = 0;
-    //virtual bool isError() = 0;
-};
